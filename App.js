dda68daf8385f24ddc7371a1eb2f56b2ac019283
Kar1004 <97450951+Kar1004@users.noreply.github.com>
2022-04-06 14:53:33 +0200

    list-fetch

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState,useEffect  } from 'react';
+import { StyleSheet, Text, View,FlatList,ScrollView} from 'react-native';
+import axios from 'axios';
+import { render } from 'react-dom';
+
+
 
 export default function App() {
+  const[countries,setCountries]=useState([]);
+  useEffect(()=>{
+    let url= "https://restcountries.com/v3.1/all";
+    fetch(url)
+    .then(res => res.json())
+    .then( res => console.log(res))
+
+    let contrie = res ;
+    setCountries(
+      contrie
+    )
+
+   
+  },[])
+
+  const Data =()=>[
+    {
+       Name : "res.name",
+    }
+    
+  ]
+  
+    
+
+  
   return (
+    <ScrollView>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+      data={Data}
+      renderItem={this.renderItem}
+      keyExtractor={(item, index) => index.toString()}
+      />
+        
+
     </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
